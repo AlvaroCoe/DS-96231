3313ade0c42486a5856341ba0b0338f976312102
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-03 21:32:12 -0300

    atualização encapsulamento

--- poo/src/main/java/com/example/encapsulamento/Aluno.java
+++ poo/src/main/java/com/example/encapsulamento/Aluno.java
@@ -6,23 +6,18 @@ public class Aluno {
 
     private String nome;
     private String email;
+   // private String Turma;
+   // private String Curso;
     
     //CONSTRUTORES
 
     public Aluno(String nome, String email) {
         this.nome = nome;
         this.email = email;
+
     }
 
     //MÉTODOS
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     public String getEmail() {
         return email;
@@ -32,6 +27,13 @@ public class Aluno {
         this.email = email;
     }
 
-    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 
 }
