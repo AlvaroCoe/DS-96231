2d74169109a64d243b9d7fbbd1b370cef2a3960a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-10 21:29:27 -0300

    herança aula 2

--- Heranca/src/main/java/org/example/exemplo1/Pessoa.java
+++ Heranca/src/main/java/org/example/exemplo1/Pessoa.java
@@ -1,14 +1,18 @@
 package org.example.exemplo1;
 
-public abstract class Pessoa {
+public abstract class Pessoa {  //abstract - "Pessoa" é uma classe que serve como modelo
 
     protected String nome;
     protected int idade;
+
+    //construtor
 
     public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
+
+    //métodos
 
     public String getNome() {
         return nome;
@@ -25,6 +29,8 @@ public abstract class Pessoa {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
+    //toString
 
     @Override
     public String toString() {
